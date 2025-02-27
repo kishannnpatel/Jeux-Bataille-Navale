8c83c903b54ced2666c22ac2363cabba6f88bbe2
kishannnpatel <149829611+kishannnpatel@users.noreply.github.com>
2025-02-27 13:04:35 +0100

    Initial commit

---
+++ src/RegleDeJeu.java
@@ -0,0 +1,10 @@
+public class regleDeJeu {
+    public static void regleDeJeu() {
+        System.out.println("Voici les règles du jeu :");
+        System.out.println("1. Le jeu se joue sur une grille de 10x10.");
+        System.out.println("2. Chaque joueur a une flotte composée de 5 navires.");
+        System.out.println("3. Le but du jeu est de couler tous les navires de l'adversaire.");
+        System.out.println("4. Vous pouvez jouer contre un autre joueur ou contre l'IA.");
+        System.out.println("5. Le jeu se termine lorsque tous les navires d'un joueur sont coulés.");
+    }
+}
